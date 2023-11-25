b245f09968521cec254e853af07a3a3ee1e1e176
Usman Afzal <muhusmanafzal@gmail.com>
2023-11-26 02:20:16 +0500

    dbconnection

---
+++ DBConnecction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace CafeManagement
+{
+    public static class DBConnecction
+    {
+         public static SqlConnection OpenConnection()
+        {
+            SqlConnection conn = new SqlConnection(@"Data Source=USMAN\SQLEXPRESS;Initial Catalog=CafeManagement;Integrated Security=True");
+            return conn;
+        }
+    }
+}
